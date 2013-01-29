7dabe3f7ab623cef9aadd80186a3a4e2c77b756a
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2013-01-29 10:24:10 +0100

    improved console output

--- js/webrtc.js
+++ js/webrtc.js
@@ -1,41 +1,71 @@
 /**
- * Variables with cross browser compatibility 
+ * Variables with cross browser compatibility
  */
 PeerConnection = (webkitRTCPeerConnection || mozRTCPeerConnection);
 navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
 /**
- * Web Real Time Communication 
+ * Web Real Time Communication
  */
 var WebRTC = {
-  configuration: {"iceServers": [{"url": "stun:provserver.televolution.net"}, {"url": "stun:stun1.voiceeclipse.net"}]},
+  configuration: {
+    "iceServers": [{
+      "url": "stun:provserver.televolution.net"
+    }, {
+      "url": "stun:stun1.voiceeclipse.net"
+    }]
+  },
   peerConnection: new PeerConnection(this.configuration),
-  init: function(){
-    //
-    this.peerConnection.onicecandidate = function(message){
-      signalingChannel.send({subject:"candidate", ice:description});
+  amountIceMessages: 0,
+  init: function() {
+    //gets ice messages from stun server?
+    this.peerConnection.onicecandidate = function(message) {
+      console.log("WebRTC: NEW ICE MESSAGE FROM STUN" + " | At second: " + new Date().getSeconds() + " | Number: " + ++WebRTC.amountIceMessages);
+
+      SignalingChannel.send({
+        subject: "candidate",
+        ice: message
+      });
     };
     //Gets called when a remote stream arrives
-    this.peerConnection.onaddstream = function(remote){
-      $('#remote-stream').attr('src', URL.createObjectURL(remote.localStream));
+    this.peerConnection.onaddstream = function(remote) {
+      console.log("WebRTC: NEW STREAM ARRIVED");
+      $('#remote-stream').attr('src', URL.createObjectURL(remote.stream));
     };
   },
-  call: function(){
-    this.peerConnection.addStream(LocalMedia.getStream());
-    
-    //when the callback gotDescription gets called, then there is passed an session description
-    this.peerConnection.createOffer(gotDescription);
+  call: function() {
+    var loop = setInterval(function() {
+      var localStream = LocalMedia.getStream();
+
+      if (localStream == null) {
+        console.log("var localStream = " + localStream);
+        return;
+      } else {
+        clearInterval(loop);
+
+        WebRTC.peerConnection.addStream(LocalMedia.getStream());
+        console.log("WebRTC: CREATEOFFER");
+        //when the callback gotDescription gets called, then there is passed an session description
+        WebRTC.peerConnection.createOffer(WebRTC.gotDescription);
+      }
+    }, 1500);
   },
-  takeOff: function(){
+  takeOff: function() {
+    console.log("WebRTC: TAKEOFF");
     //the own session description has to be compatible to the remote session description
     //so createAnswer needs the remote session description also for creating the local description
     this.peerConnection.createAnswer(peerConnection.remoteDescription, gotDescription);
+
   },
   //this function is used from createAnswer and createOffer
   //because both have the an session description for local purpose as result
-  gotDescription: function(description){
-    peerConnection.setLocalDescription(description);
-    signalingChannel.send({subject:"sdp", sdp:description});
+  gotDescription: function(description) {
+    console.log("WebRTC: GOTDESCRIPTION " + description);
+    WebRTC.peerConnection.setLocalDescription(description);
+    SignalingChannel.send({
+      subject: "sdp",
+      sdp: description
+    });
   }
 };
 
@@ -44,15 +74,17 @@ var WebRTC = {
  */
 var SignalingChannel = {
   webSocket: null,
-  init: function(){
+  init: function() {
     var self = this;
     this.webSocket = new WebSocket('ws://localhost:9001');
-    this.webSocket.onopen = function(){
-      self.send("");
+    this.webSocket.onopen = function() {
+      console.log("WebSocket: ONOPEN");
+      self.send("ONOPEN");
     };
-    this.webSocket.onmessage = function(message){
+    this.webSocket.onmessage = function(message) {
+      console.log("WebSocket: ONMESSAGE (from WebSocketServer)");
       message = JSON.parse(message);
-      switch(message.subject){
+      switch(message.subject) {
         case "sdp":
           WebRTC.peerConnection.setRemoteDescription(new RTCSessionDescription(message.sdp));
           WebRTC.takeOff();
@@ -63,33 +95,35 @@ var SignalingChannel = {
         default:
           break;
       };
-      
-      console.log("WebSocket: ONMESSAGE");
     };
-    this.webSocket.onerror = function(error){
+    this.webSocket.onerror = function(error) {
       console.log("WebSocket: ERROR");
     };
-    this.webSocket.onclose = function(){
+    this.webSocket.onclose = function() {
       console.log("WebSocket: CLOSE");
     };
   },
-  send: function(message){
+  send: function(message) {
+    console.log("WebSocket: SEND " + message);
     this.webSocket.send(message);
   }
 };
 
 /**
- * Local Media Stream 
+ * Local Media Stream
  */
 var LocalMedia = {
   localStream: null,
-  getStream: function(){
+  setStream: function(stream) {
+    this.localStream = stream;
+  },
+  getStream: function() {
     return this.localStream;
   },
   onSuccess: function(localStream) {
-    this.localStream = localStream;
-    $('#local-stream').attr('src', URL.createObjectURL(this.localStream));
     console.log("LocalMedia: SUCCESS");
+    LocalMedia.setStream(localStream);
+    $('#local-stream').attr('src', URL.createObjectURL(localStream));
   },
   onError: function(error) {
     console.log("LocalMedia: ERROR " + error);
@@ -112,4 +146,4 @@ WebRTC.init();
 SignalingChannel.init();
 
 LocalMedia.start();
-WebRTC.start();
+WebRTC.call();
