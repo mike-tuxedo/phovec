aafd7d4f999da9033ec195ed7355d6791e4bbc28
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2013-02-25 11:40:15 +0100

    added user_controller (user.js) and modified room.js

--- js/controllers/room.js
+++ js/controllers/room.js
@@ -1,3 +1,21 @@
 ﻿App.RoomController = Ember.ObjectController.extend({
-
+	ready:function(){
+		if (webkitRTCPeerConnection) {
+		  PeerConnection = webkitRTCPeerConnection;
+		} else if (mozRTCPeerConnection) {
+		  PeerConnection = mozRTCPeerConnection;
+		}
+		
+		if (navigator.getUserMedia) {
+		  navigator.getMedia = navigator.getUserMedia;
+		} else if (navigator.webkitGetUserMedia) {
+		  navigator.getMedia = navigator.webkitGetUserMedia;
+		} else if (navigator.mozGetUserMedia) {
+		  navigator.getMedia = navigator.mozGetUserMedia;
+		} else if (navigator.msGetUserMedia) {
+		  navigator.getMedia = navigator.msGetUserMedia;
+		}
+		
+		App.user.start();
+	}	
 });
---
+++ js/controllers/user.js
@@ -0,0 +1,35 @@
+﻿App.UserController = Ember.ObjectController.extend({
+
+	  localStream: null,
+	  setStream: function(stream) {
+	    this.localStream = stream;
+	  },
+	  getStream: function() {
+	    return this.localStream;
+	  },
+	  onSuccess: function(localStream) {
+	    console.log()
+	    this.setStream(localStream);
+	    window.dispatchEvent(new CustomEvent("localmedia:available"));
+	
+	    $('#local-stream').attr('src', URL.createObjectURL(localStream));
+	  },
+	  onError: function(error) {
+	    console.log("LocalMedia: ERROR");
+	    console.log(error);
+	  },
+	  start: function() {
+	  	console.log('try to get local videostream...');
+	    //request audio and video from your own hardware
+	    navigator.getMedia({
+	      audio: true,
+	      video: true
+	    }, this.onSuccess, this.onError);
+	  },
+	  stop: function() {
+	    //get(0) gets the dom element fromt he jquery selector
+	    $('#local-stream').get(0).pause();
+	    $('#local-stream').attr('src', null);
+	  }
+
+});
