08a05be598ca843eee8a70ee4cb7bfe59dc9f7d1
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2013-03-16 20:39:25 +0100

    audio und video mute/unmute

--- js/controllers/user.js
+++ js/controllers/user.js
@@ -73,5 +73,11 @@
     console.log('sendMail called');
     if( mail_sett.from && mail_sett.to && mail_sett.subject && mail_sett.text && mail_sett.html )
       SignalingChannel.send({ subject: 'mail', chatroomHash: WebRTC.users[0].roomHash, userHash: WebRTC.users[0].id, mail: { from: mail_sett.from, to: mail_sett.to, subject: mail_sett.subject, text: mail_sett.text, html: mail_sett.html } })
+  },
+  mouseIn: function(e){
+    e.children('.videoOptions').fadeIn();
+  },
+  mouseOut: function(e){
+    e.children('.videoOptions').fadeOut();
   }
 });
