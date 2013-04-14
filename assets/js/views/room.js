55c9fb5180c49254a01795d61f6dc0ad27229544
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2013-04-14 20:41:02 +0200

    change sidebar behavior

--- assets/js/views/room.js
+++ assets/js/views/room.js
@@ -4,9 +4,7 @@
   sidebar: false,
   showSidebar: function(){
       if(this.sidebar === false){
-        $('#social_sidebar').animate({ right: '0px'}, { duration: 250, queue: false });
-        $('#show_sidebar').animate({ right: '155px', opacity: '0.3'}, { duration: 250, queue: false });
-        $('#show_sidebar_shadow').animate({ right: '155px'}, { duration: 250, queue: false });
+        $('#social_sidebar_container').animate({ right: '0px'}, { duration: 250, queue: false });
         
         $('#show_sidebar').mouseover(function(){$('#show_sidebar').css('opacity', '1')});
         $('#show_sidebar').mouseout(function(){$('#show_sidebar').css('opacity', '0.3')});
@@ -14,9 +12,7 @@
         this.sidebar = true;
       }
       else{
-        $('#social_sidebar').animate({ right: '-190px'}, { duration: 250, queue: false });
-        $('#show_sidebar').animate({ right: '-35px'}, { duration: 250, queue: false });
-        $('#show_sidebar_shadow').animate({ right: '-35px'}, { duration: 250, queue: false });
+        $('#social_sidebar_container').animate({ right: '-180px'}, { duration: 250, queue: false });
         
         $('#show_sidebar').mouseout(function(){$('#show_sidebar').css('opacity', '1')});
         $('#show_sidebar').val('<');
