c3b806639c6cc5983dfda417cc18d94949181f68
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2013-05-22 13:28:32 +0200

    made the app full responsive.. YEAH

--- assets/js/views/user.js
+++ assets/js/views/user.js
@@ -1,6 +1,8 @@
 App.UserView = Ember.View.extend({
   didInsertElement: function(){
     $('#videoboxes')[0].addEventListener('click',App.Controller.room.handleClickEvent,true);
+
+    App.Controller.user.setWindowWidth();
   },
   controlEffects: function(){
     $('#videoEffects').css('display', 'block');
