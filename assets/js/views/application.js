45175462a3ab63098366fab02a42334cde5b4ee4
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2013-06-04 12:56:22 +0200

    bug-fix for isNameFormActivated

--- assets/js/views/application.js
+++ assets/js/views/application.js
@@ -1,16 +1,14 @@
 App.ApplicationView = Ember.View.extend({
   templateName: 'application',
   classNames: ['wrapper'],
-  click: function(e){
+  click: function(e) {
     var clickedElement = e.target;
     // if user double clicked on his name and clicks outside without pressing enter
-    if( App.Controller.room.isNameFormActivated(clickedElement) ){
-      $('#videoboxes form#alterNameForm').submit()
+    if (App.Controller.room !== undefined) {
+      if (App.Controller.room.isNameFormActivated(clickedElement)) {
+        $('#videoboxes form#alterNameForm').submit()
+      }
     }
   }
 });
-
-
-
-
 
